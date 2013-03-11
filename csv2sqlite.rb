8bb0e6ccfc9182118b653d6372db2c47a7af8bab
Alex Dergachev <alex@evolvingweb.ca>
2013-03-11 15:05:10 -0400

    Further improvements

--- csv2sqlite.rb
+++ csv2sqlite.rb
@@ -4,7 +4,7 @@ require 'csv'
 require 'rubygems'
 require 'sequel'
 require 'trollop'
-require 'irb'
+require 'tempfile'
 
 OPTIONS = Trollop::options do
   banner <<-EOS
@@ -18,73 +18,100 @@ EOS
   opt :output,  "FILENAME.db where to save the sqlite database", :type => :string
 end
 
-def getDatabase(filename = nil)
-  if filename
-    puts "Connecting to sqlite://#{filename}"
-    database = Sequel.sqlite(OPTIONS[:output])
-    database.test_connection # saves blank file
-  else
-    puts "Connecting to sqlite://:memory:"
-    database = Sequel.sqlite(':memory:')
-  end
+def getDatabase(filename)
+  puts "Connecting to sqlite://#{filename}"
+  database = Sequel.sqlite(filename)
+  # database.test_connection # saves blank file
   return database
 end
 
 def populateTableFromCSV(database,filename)
-
-  #   - The CSV has a header line, if not look at :headers option in CSV.new
-  #     http://ruby-doc.org/stdlib-1.9.3/libdoc/csv/rdoc/CSV.html#method-c-new
   options = { :headers    => true,
               :header_converters => :symbol,
               :converters => :all  }
-  rows = []
-  CSV.foreach(filename, options) do |row|
-      rows.push(Hash[row.headers.zip(row.fields)])
-  end
-
-  headers = rows[0].keys
-
+  data = CSV.table(filename, options)
+  headers = data.headers
   tablename = File.basename(filename, '.csv').gsub(/[^0-9a-zA-Z_]/,'_').to_sym
 
   puts "Dropping and re-creating table #{tablename}"
   DB.drop_table? tablename
   DB.create_table tablename do
+    # see http://sequel.rubyforge.org/rdoc/files/doc/schema_modification_rdoc.html
     # primary_key :id
     # Float :price
-    headers.each do |col|
-      String col
+    data.by_col!.each do |columnName,rows|
+      columnType = getCommonClass(rows) || String
+      column columnName, columnType
     end
   end
-  rows.each do |row|
-    database[tablename].insert(row)
+  data.by_row!.each do |row|
+    database[tablename].insert(row.to_hash)
   end
 end
 
-def launchConsole(database)
-  require 'pp'
-  require 'irb'
+# 
+# :call-seq:
+#   getCommonClass([1,2,3], String)         => FixNum
+#   getCommonClass([1,"bob",3], String)     => String
+#
+# Returns the class of each element in +rows+ if same for all elements, otherwise returns nil
+#
+def getCommonClass(rows)
+  return rows.inject(rows[0].class) { |klass, el| break if klass != el.class ; klass }
+end
 
+
+def launchConsole(database)
+  require 'irb'
+  require 'pp'
+  require 'yaml'
+
+  puts "Launching IRB Console.\n\n"
   puts "You can now interact with the database via DB. Examples:"
   puts "  DB.tables #=> SHOW tables"
   puts "  ds = DB[:posts] #=> SELECT * FROM posts"
   puts "  ds = DB[:posts].where(:id => 1) #==> SELECT * FROM posts WHERE id => 1"
   puts "  puts DB[:posts].all ; nil #=> executes query, pretty prints results" 
+  puts ""
   puts "See http://sequel.rubyforge.org/rdoc/files/doc/dataset_basics_rdoc.html"
-  puts "Type 'sqlite3' to enter sqlite3 console"
+  puts "To launch sqlite3 console, type 'sqlite3'"
+  puts ""
 
   puts "Available tables: "
   database.tables.each do |table|
-    puts " #{table.to_s} - #{DB[table].count.to_s} records"
+    puts "  DB[:#{table.to_s}] - #{DB[table].count.to_s} records"
   end
+  puts ""
+
   IRB.start
-  exit;
+  catch :IRB_EXIT do
+    # IRB.start should trap this but doesn't
+    exit;
+  end
 end
 
-def launchSqliteConsole(filename)
-  Kernel.exec('sqlite3 ' + filename)
+def sqlite3()
+  launchSqliteConsole()
 end
 
-DB = getDatabase(OPTIONS[:output])
+def launchSqliteConsole()
+  File.exists?(DB_PATH) or Trollop.die "Unable to launch sqlite3; invalid file: #{DB_PATH}" 
+  puts "Launching 'sqlite3 #{DB_PATH}'. Table schema:\n"
+  # NB: Using Kernel.system instead of Kernel.exec to allow Tempfile cleanup
+  system("sqlite3 #{DB_PATH} '.schema'")
+  puts ""
+  system("sqlite3 #{DB_PATH}")
+  exit
+end
+
+if OPTIONS[:output]
+  DB_PATH = OPTIONS[:output]
+else
+  DB_TMP = Tempfile.new(['csv2sqlite','.sqlite3'])
+  DB_PATH = DB_TMP.path
+end
+
+DB = getDatabase(DB_PATH)
 
 Trollop.die "Missing CSV file argument(s)" unless ARGV.count > 0
 until ARGV.empty? do 
@@ -94,19 +121,17 @@ until ARGV.empty? do
   populateTableFromCSV(DB, file)
 end
 
-launchConsole(DB) if OPTIONS[:irb_console]
-if OPTIONS[:sqlite_console] 
-  Trollop.die "--sqlite-console requires --output" unless OPTIONS[:output]
-  launchSqliteConsole(OPTIONS[:output]) 
-end
+launchSqliteConsole() if OPTIONS[:sqlite_console] 
+launchConsole(DB) if OPTIONS[:irb_console] || ! OPTIONS[:output]
 
 __END__
-
-$ ruby search-csv.rb
-year: 1911           name: Ruby           percent: 0.007637    sex: girl
-
-* This assumes Ruby 1.9's CSV library, if you are using 1.8, use FasterCSV.
-* http://sequel.rubyforge.org/rdoc/classes/Sequel/Database.html#method-i-drop_table
-
-
-  
+"year","name","percent","sex"
+1880,"John",0.081541,"boy"
+1880,"William",0.080511,"boy"
+1880,"James",0.050057,"boy"
+1880,"Charles",0.045167,"boy"
+1880,"George",0.043292,"boy"
+1880,"Frank",0.02738,"boy"
+1880,"Joseph",0.022229,"boy"
+1880,"Thomas",0.021401,"boy"
+1880,"Henry",0.020641,"boy"
